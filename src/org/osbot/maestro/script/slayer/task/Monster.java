edb3f5dde302a291fbf8f4bae789d9509aaa170d
Bautista Baiocchi-Lora <Bautista-Baiocchi-lora@users.noreply.github.com>
2018-01-03 04:00:21 -0300

    Added earth warriors.

--- src/org/osbot/maestro/script/slayer/task/Monster.java
+++ src/org/osbot/maestro/script/slayer/task/Monster.java
@@ -8,7 +8,7 @@ public enum Monster {
 
     ROCKSLUGS("Rockslug", MonsterMechanic.ROCK_SLUG_MECHANIC, RequiredInventoryItem.BAG_OF_SALT), CAVE_CRAWLERS("Cave " +
             "crawler", null, RequiredInventoryItem.ANTIDOTE), OGRES("Ogre", null), KALPHITES("Kalphite", null), HILL_GIANTS("Hill Giant", null),
-    MOSS_GIANTS("Moss giant", null), CAVE_SLIMES("Cave slime", null);
+    MOSS_GIANTS("Moss giant", null), CAVE_SLIMES("Cave slime", null), EARTH_WARRIORS("Earth warrior", null);
 
     private final String name;
     private final MonsterMechanic monsterMechanic;
